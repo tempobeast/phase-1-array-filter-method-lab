5f39bbf95d250201bbf7ea3a2854b476e6209793
Zach <shaw.zacht@gmail.com>
2022-04-05 22:51:21 -0700

    commit tree 0ac3b59e8c4ad3e7ac8518ebc3bdfaf1cde20db8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+    const name = array.filter(item => item.toLowerCase() === string.toLowerCase())
+    return name
+}
+
+function fuzzyMatch(array, string) {
+    const firstInit = array.filter(item => item.charAt(0) === string.charAt(0))
+    return firstInit
+}
+
+function matchName(array, string) {
+    const match = array.filter(item => item.name.toLowerCase() === string.toLowerCase())
+    return match
+}
